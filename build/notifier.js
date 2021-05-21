eb944b8b8e1d5e41a55f2275df047eb8728e8c33
Jason Lee <j@jlee.me>
2021-05-21 06:27:29 +0000

    fixed up debug stuff

--- build/notifier.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.notify = void 0;
-const notifier = require('node-notifier');
-function notify(...args) {
-    notifier.notify(...args);
-}
-exports.notify = notify;
--- build/pricy.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const debug_1 = require("./debug");
-console.log('starting up');
-debug_1.debug('this is a test');
