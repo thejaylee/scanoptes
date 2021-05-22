62b081f96e51d4183b67036ce96e31056d6ff9c3
Jason Lee <j@jlee.me>
2021-05-22 00:44:26 +0000

    changed up module types and resolution

--- src/pricy-monitor.ts
+++ src/pricy-monitor.ts
@@ -1,8 +1,15 @@
-import debug from './debug';
-import { notify } from './notifier';
+import debug from './debug.js';
+import { notify } from './notifier.js';
+import http from 'http';
+import https from 'https';
 
 process.on('unhandledRejection', console.log);
 
 const NODE_ENV : string | undefined = process.env.NODE_ENV;
 
 debug.info('starting up');
+
+// https://www.amazon.ca/Sony-SEL1224G-12-24mm-Fixed-Camera/dp/B072J4B6WS/
+//let res: http.ClientRequest = await https.get('https://google.ca');
+//debug.trace(res);
+debug.info('ending');
